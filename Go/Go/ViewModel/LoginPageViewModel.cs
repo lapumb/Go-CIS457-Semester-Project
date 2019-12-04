fd32dc0a1f83e3bf5a199d431a4cf52840b6af9e
Blake Lapum <lapumb@mail.gvsu.edu>
2019-12-04 12:01:29 -0500

    loading dialog on login

--- Go/Go/ViewModel/LoginPageViewModel.cs
+++ Go/Go/ViewModel/LoginPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Go.Model;
+﻿using Acr.UserDialogs;
+using Go.Model;
 using Go.View;
 using System;
 using System.ComponentModel;
@@ -21,6 +22,7 @@ namespace Go.ViewModel
         {
             try
             {
+                UserDialogs.Instance.ShowLoading("Logging in..", null);
                 var result = await App.FirebaseClient.GetAllUsers();
                 foreach (UserInfo info in result)
                 {
@@ -29,6 +31,7 @@ namespace Go.ViewModel
                         if (info.Password.Equals(UserInfo.User.Password))
                         {
                             App.Master.Detail = new NavigationPage(new MainPage());
+                            UserDialogs.Instance.HideLoading();
                             return;
                         }
                     }
@@ -36,11 +39,13 @@ namespace Go.ViewModel
             } 
             catch (Exception e)
             {
+                UserDialogs.Instance.HideLoading();
                 Debug.WriteLine("LPVM/LS, caught : " + e.Message);
                 Utilities.Utilities.DisplayMessage("Uh Oh..", "Unexpected error occured. Please check connection.");
                 return;
             }
 
+            UserDialogs.Instance.HideLoading();
             await App.LoginPg.DisplayAlert("Error", "The username and/or password you entered is not correct. Please retry.", "Okay"); 
         }
 
@@ -73,7 +78,8 @@ namespace Go.ViewModel
                     UserInfo result = null; 
                     if (UserInfo.User.Username.Contains("Guest"))
                         await App.LoginPg.DisplayAlert("No!", "Your username cannot contain the word 'guest'. Please enter something else.", "Okay");
-
+                    
+                    UserDialogs.Instance.ShowLoading("Creating user..");
                     try
                     {
                         result = await App.FirebaseClient.GetUser(UserInfo.User.Username);
@@ -90,10 +96,10 @@ namespace Go.ViewModel
                         try
                         {
                             await App.FirebaseClient.AddUser(UserInfo.User.Username, UserInfo.User.Password);
-                            await App.LoginPg.DisplayAlert("Success!", "Account created.", "Okay");
                         }
                         catch (Exception e)
                         {
+                            UserDialogs.Instance.HideLoading();
                             Debug.WriteLine("LPVM/CAC, Exception Caught when creating account: " + e.Message);
                             await App.LoginPg.DisplayAlert("Uh oh..", "Failed creating account. Please check connection.", "Okay");
                             return;
@@ -101,6 +107,7 @@ namespace Go.ViewModel
                     }
                     else
                     {
+                        UserDialogs.Instance.HideLoading();
                         await App.LoginPg.DisplayAlert("No!", "This username already exists. Please use another.", "Okay");
                         return;
                     }
--- Go/Go/ViewModel/MainPageViewModel.cs
+++ Go/Go/ViewModel/MainPageViewModel.cs
@@ -31,7 +31,7 @@ namespace Go.ViewModel
         public ICommand ToGameCommand => new Command(async () =>
         {
             //TODO: needs to be your own IP address
-            EndPoint = Utilities.Utilities.SetIpAddress("192.168.1.3");     //192.168.1.7
+            EndPoint = Utilities.Utilities.SetIpAddress("35.40.135.68");     //192.168.1.7
             //EndPoint = Utilities.Utilities.GetNetwork(); 
             Debug.WriteLine("Set Netowrk EndPoint (Connection) to : " + EndPoint.ToString()); 
             string[] btns =
